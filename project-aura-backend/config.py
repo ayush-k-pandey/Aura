ea565be40ea7650bd84f316759e9aae15b1d93b3
Harsh <harsh9934530656pathak@gmail.com>
2025-09-02 15:43:36 +0530

    harsh final backend

--- project-aura-backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Local PostgreSQL Database URL
-# Replace 'your_username' and 'your_password' with your local PostgreSQL credentials
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/aura_db"
